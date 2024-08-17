cbf358351a416036999e3c70b94d6a1e43123de5
abdulgilani <ah-gilani@hotmail.com>
2024-08-17 17:46:35 -0400

    Added primary and secondary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "22C55E",
+        secondary: "EF4444",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
